1391df7253a6dbd3e4b93c214b625ac25d74d469
PedroPK <pedropk@gmail.com>
2022-07-27 09:56:22 -0300

    Global changes in Style, Default Template Content and Module Imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'a11y-p1';
+	title = 'a11y-p1';
+
+	public yesNoAnswer = 'no';
 }
