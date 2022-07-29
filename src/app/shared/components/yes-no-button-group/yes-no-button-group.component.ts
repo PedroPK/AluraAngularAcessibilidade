d5e2cf9e907a7f904f8545d29f4a5d0a69b8c2ff
PedroPK <pedropk@gmail.com>
2022-07-28 21:02:29 -0300

    01) Implement the ControlValueAccessor Interface This adds the following methods:  - writeValue()  - registerOnChange()  - registerOnTouched()  - setDisabledState
    02) Using NG_VALUE_ACCESSOR as provide

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,11 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output }	from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR }			from '@angular/forms';
 
 @Component({
 	selector: 'app-yes-no-button-group',
 	templateUrl: './yes-no-button-group.component.html',
 	styleUrls: ['./yes-no-button-group.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			multi: true,
+			useExisting: forwardRef( () => YesNoButtonGroupComponent)
+		}
+	]
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
 
 	@Input()
 	public value: string = null;
@@ -17,14 +25,38 @@ export class YesNoButtonGroupComponent implements OnInit {
 	public valueChange = new EventEmitter<string>();
 
 	public options = YesNoButtonGroupOptions;
+	public onChange = (pValue: string) => {};
+	public onTouched = () => {};
 
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		//
+	}
 
 	public activate(pValue: string): void {
+		this.writeValue(pValue);
+	}
+
+	public writeValue(pValue: string): void {
+		// Write on View when Model is changed
 		this.value = pValue;
+		this.onChange(this.value);
 		this.valueChange.emit(this.value);
+	}
+
+	public registerOnChange(pFunction: (value: string) => void): void {
+		// Allow us to receive in any moment in the future a Function to be invoked OnChange
+		this.onChange = pFunction;
+	}
+
+	public registerOnTouched(pFunction: () => void): void {
+		// Allow us to receive in any moment in the future a Function to be invoked OnTouched
+		this.onTouched = pFunction;
+	}
+
+	public setDisabledState?(isDisabled: boolean): void {
+		throw new Error('Method not implemented.');
 	}
 
 }
