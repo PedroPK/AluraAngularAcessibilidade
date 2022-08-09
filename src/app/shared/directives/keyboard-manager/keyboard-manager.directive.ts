02ff0ddc77cbc63e268c57934a290c8ae2469ed0
PedroPK <pedropk@gmail.com>
2022-08-08 21:58:41 -0300

    Changing the Focus using Left and Right keys

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,6 +1,13 @@
 import { ContentChildren, Directive, HostListener, QueryList } from '@angular/core';
 import { KeyboardManagedItemDirective } from './keyboard-managed-item.directive';
 
+enum ArrowDirection {
+
+	LEFT = -1,
+	RIGHT = 1,
+
+}
+
 @Directive({
 	selector: '[appKeyboardManager]',
 })
@@ -15,7 +22,7 @@ export class KeyboardManagerDirective {
 	public manageKeys(event: KeyboardEvent): void {
 		switch (event.key) {
 			case 'ArrowUp':
-				console.log(`QuertList<KeyboadManagedItensDirective> = ${this.items}`)
+				//console.log(`QuertList<KeyboadManagedItensDirective> = ${this.items}`)
 				console.log('Up!');
 				break;
 
@@ -24,13 +31,51 @@ export class KeyboardManagerDirective {
 				break;
 
 			case 'ArrowLeft':
+				this.moveFocus(ArrowDirection.LEFT).focus();
 				console.log('Left!');
 				break;
 
 			case 'ArrowRight':
+				this.moveFocus(ArrowDirection.RIGHT).focus();
 				console.log('Right!');
 				break;
 
 		}
 	}
+
+	public moveFocus(direction: ArrowDirection): KeyboardManagedItemDirective {
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> direction = ${direction}`);
+
+		const items = this.items.toArray();
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> this.items.toArray().length = ${items.length}`);
+
+		const currentSelectedIndex = items.findIndex(item => item.isFocused());
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> currentSelectedIndex = ${currentSelectedIndex}`);
+
+		let nextIndex = currentSelectedIndex + direction;
+		if ( nextIndex >= items.length ) {
+			nextIndex = 0;
+		}
+
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> nextIndex = ${nextIndex}`);
+		const targetElementFocus = items[nextIndex];
+
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> targetElementFocus = ${targetElementFocus}`);
+
+		//console.log(' ');
+		if ( targetElementFocus ) {
+			return targetElementFocus;
+		} else {
+			return direction === ArrowDirection.LEFT
+				? items[items.length - 1]
+				: items[0]
+			;
+		}
+
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> items.length = ${items.length}`);
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> items.length - 1 = ${items.length - 1}`);
+		//console.log(`Keyboard Manager Directive >> moveFocus() >> targetElementFocus = ${targetElementFocus}`);
+
+	}
+
 }
